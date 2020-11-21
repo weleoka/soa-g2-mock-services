620e0296f5541eb7f3d4e52b53c144381819d686
Kai Weeks <kai.weeks@gmail.com>
2020-11-21 17:42:15 +0000

    Names and student_id is now better fakes

--- src/studentits/students.js
+++ src/studentits/students.js
@@ -4,14 +4,24 @@ const casual = require('casual').sv_SE
 module.exports = () => {
     casual.define('students', function(i) {
         const ssnArr = [ "19890812-1234", "19900912-4321", "19911012-7890"];
-        const usernameArr = ["ingand-4", "tompet-8", "amwi-7"];
+        //const studentIdArr = ["ingand-4", "tompet-8", "amwi-7"];
+        //const firstNameArr = ["Ingvar", "Tom", "Amy"];
+        //const lastNameArr = ["Andromeda", "Petty", "Winner"];
 
         const ssn = ssnArr[i];
-        const username = usernameArr[i];
+        //const studentId = studentIdArr[i];
+        const firstName = casual.first_name;
+        const lastName = casual.last_name
+        const studentId = firstName.slice(0, 2).concat(
+            lastName.slice(0, 2), "-", Math.floor(Math.random() * 10));
 
         return {
             ssn: ssn,
-            username: username,
+            // username is legacy: replaced by student_id
+            username: studentId.toLowerCase(),
+            student_id: studentId.toLowerCase(),
+            first_name: firstName,
+            last_name: lastName
         }
     })
 
