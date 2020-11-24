911995e090eab54c046a99be605551b6048e3377
Simon Blomsterlund <simon@simonblund.com>
2020-11-24 23:35:04 +0200

    Fix ids on results to enable posts

--- src/ladok/results.js
+++ src/ladok/results.js
@@ -11,12 +11,14 @@ module.exports = (ssnArrEx) => {
         const moduleIdArr = ["module01", "module02", "module03", "module04",  "module05", "module06"];
         const gradeArr = [ "U", "G", "VG"];
 
+        const id = Math.floor(Math.random() * 1000);
         const ssn = ssnArr[Math.floor(Math.random() * ssnArr.length)];
         const courseCode = courseCodeArr[Math.floor(Math.random() * courseCodeArr.length)];
         const modul = moduleIdArr[Math.floor(Math.random() * moduleIdArr.length)];
         const grade = gradeArr[Math.floor(Math.random() * gradeArr.length)];
 
         return {
+            id: id,
             ssn: ssn,
             course_code: courseCode, // This should be dropped.
             module: modul,
